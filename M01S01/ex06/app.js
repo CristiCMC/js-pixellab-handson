e232400e7a5c7f2278037458d58f0f91258adebb
CristiCMC <100723697+CristiCMC@users.noreply.github.com>
2022-07-11 20:25:49 +0100

    Tema java 01

---
+++ M01S01/ex06/app.js
@@ -0,0 +1,14 @@
+var InputRadius = document.getElementById('radius');
+var elementResult = document.getElementById('result');
+var form = document.querySelector('form');
+
+form.addEventListener('submit', function (event) {
+  var radius = InputRadius.value || 0;
+  var result = 0;
+
+  result = 4 * Math.PI * Math.pow(7, 2);
+
+  elementResult.innerText = result;
+
+  event.preventDefault();
+});
--- M01S02/ex01/shapes.js
+++ M01S02/ex01/shapes.js
@@ -1,1 +1,2 @@
 console.log(Math.pow(16, 2).toString());
+console.log((20 * 12).toString());
